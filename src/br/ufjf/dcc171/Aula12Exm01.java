1df03b0ce90ffa629678f996fc0af21f651bf09c
ice <ice@lab06m26.nrc.ice.ufjf.br>
2017-10-10 21:55:16 -0300

    Aula 12

---
+++ src/br/ufjf/dcc171/Aula12Exm01.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula12Exm01 {
+
+    public static void main(String[] args) {
+        JanelaEditor janela = new JanelaEditor();
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+
+}
